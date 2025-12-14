94baa6532e51ca0948906217ad5b895d1928433d
Panithan Champahom <doedoserpc@gmail.com>
2025-12-15 03:03:11 +0700

    Add blank pages

---
+++ app/handbook/page.tsx
@@ -0,0 +1,9 @@
+export default function NewsPage() {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center bg-gray-50">
+      <h1 className="text-3xl font-bold text-gray-400 uppercase tracking-widest">
+        Handbook Page
+      </h1>
+    </div>
+  );
+}
